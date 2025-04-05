4b3831e0eea770f3cc87ab3a10e85379da55053a
nodirbek2209 <n.rejabaliyev@newuu.uz>
2025-04-05 23:11:04 +0500

    first commit

--- src/main/java/com/example/weblab/controller/DepartmentController.java
+++ src/main/java/com/example/weblab/controller/DepartmentController.java
@@ -8,7 +8,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/api/departments")
+@RequestMapping("/api/department")
 public class DepartmentController {
 
     private final DepartmentService service;
